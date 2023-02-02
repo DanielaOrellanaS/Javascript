8a721992b4fcc7e0411cb477d886664d8677e94c
Daniela Estefania Orellana Soriano <63482841+DanielaOrellanaS@users.noreply.github.com>
2023-02-02 08:51:05 -0500

    Ejemplo ejercicio Javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+//CONDICIONALES 
+let num1 = 6; 
+
+let answer = num1 == 6; 
+console.log(answer);
+if(num1 == 6){
+    console.log("Si es igual");
+} else {
+    console.log("No es igual");
+}
+
+let count = 0;
+while(count < 5){
+    console.log("hola");
+    count ++;
+}
+
+for (let i = 0; i < 10; i++){
+    console.log("hola 2");
+}
+
+function multiply(num1, num2){
+    let resultado = num1*num2;
+    console.log(resultado);
+}
+
+multiply(5,4);
